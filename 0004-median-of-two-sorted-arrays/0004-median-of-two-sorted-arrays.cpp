2997c2f1c5c104fa2a47a1e624cfa6d57d16115c
abhinav-2102 <132653733+abhinav-2102@users.noreply.github.com>
2025-03-28 23:03:10 +0530

    Time: 0 ms (100%), Space: 94.9 MB (93.56%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,21 +1,63 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-         vector<int>v;
+       int n1=nums1.size();
+        int n2=nums2.size();
+        int n=n1+n2;
+         
+      if(n1>n2)  return findMedianSortedArrays(nums2,nums1);
         
-        // store the array in the new array
-        for(auto num:nums1)   // O(n1)
-            v.push_back(num);
+     // When length is even, let's say 10 then left half length should be: (10+1)/2 =>5
+     // When length is odd, let's say 11 then left half length should be: (11+1)/2 =>6
+        // This mean that this formula gonna work in both condition
+        int partition=(n+1)/2; 
         
-        for(auto num:nums2)  // O(n2)
-            v.push_back(num);
+    
+    // Edge Case
+    if(n1==0)
+        return n2%2?nums2[n2/2]:(nums2[n2/2]+nums2[n2/2-1])/2.0;
+    
+    if(n2==0)
+        return n1%2?nums1[n1/2]:(nums1[n1/2]+nums1[n1/2-1])/2.0;
+    
+    // Now do Partioning
+    int left1=0;
+    int right1=n1;
+    int cut1,cut2;
+    int l1,r1,l2,r2;
+    
+    do
+    {   
+        //Findout 'cut1' and 'cut2'
+        cut1=(left1+right1)/2;
+        cut2=partition-cut1;
+   
+        // Calculation for l1
+        l1=cut1==0?INT_MIN:nums1[cut1-1];
         
-        // Sort the array to find the median
-        sort(v.begin(),v.end());  // O(nlogn)
+        // Calculation for l2
+        l2=cut2==0?INT_MIN:nums2[cut2-1];
         
-        // Find the median and Return it
-        int n=v.size();  // O(n)
+        // Calculation for r1
+        r1=cut1>=n1?INT_MAX:nums1[cut1];
         
-        return n%2?v[n/2]:(v[n/2-1]+v[n/2])/2.0;
+        // Calculation for r2
+        r2=cut2>=n2?INT_MAX:nums2[cut2];
+        
+        if(l1<=r2&&l2<=r1)
+             // Return Result
+             return n%2?max(l1,l2):(max(l1,l2)+min(r1,r2))/2.0;
+        else
+            
+        if(l1>r2)
+            right1=cut1-1;
+        else
+             left1=cut1+1;
+       
+       
+    }while(left1<=right1);
+        
+             
+    return 0.0;
     }
 };
